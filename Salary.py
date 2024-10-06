38e98377ac6b989562f2c347422f3de326ba5027
joliveira530 <o.justin530@gmail.com>
2024-10-06 17:08:27 -0400

    Add files via upload

---
+++ Salary.py
@@ -0,0 +1,19 @@
+# input statements
+salary = float(input())
+numDependents = float(input())
+
+# calculate taxes here
+stateTax = salary * 0.065
+federalTax = salary * 0.28
+dependentDeduction = (numDependents * 0.025) * salary
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
+
+
+# output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tex: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
+
+print("Salary: $" + str(salary))
+print("Take Home Pay: $" + str(takeHomePay))
